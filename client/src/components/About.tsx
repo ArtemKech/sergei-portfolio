07b40092f0ba6df343b29750bba4ae16cd652b68
ArtemKech <kecha1@student.op.ac.nz>
2024-02-26 23:32:34 +1300

    fix(client): minor readjustments for design

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "../styles/About.css";
 import prof_pic_nb from "../assets/About/pfptouse.png";
+import first_pic_collage from "../assets/About/2nd_pic_collage.png";
+import second_pic_collage from "../assets/About/1st_pic_collage.png";
+import third_pic_collage from "../assets/About/3rd_pic_collage.png";
 
 const About: React.FC = () => {
   return (
@@ -20,6 +23,40 @@ const About: React.FC = () => {
           </p>
         </div>
         <img src={prof_pic_nb} alt="pfp_nb" className="pfp_nb" />
+      </div>
+      <div className="second_bio_container">
+        <div className="second_bio">
+          <h2 className="second_bio_title">Моя история</h2>
+          <p className="second_bio_description">
+            Вырос в городе Москва, где и начал свою карьеру фотографа. Мои
+            работы были опубликованы в различных журналах и книгах, а также
+            использовались в рекламных кампаниях и на обложках альбомов. В
+            настоящее время я работаю в различных жанрах, таких как портретная,
+            свадебная, репортажная и архитектурная фотография. Мои работы
+            отличаются своей яркостью и оригинальностью, а также тем, что они
+            всегда передают атмосферу момента, который является ключевым в моей
+            работе.
+          </p>
+        </div>
+        <div className="collage_for_2nd_bio">
+          <img
+            src={first_pic_collage}
+            alt="1st_pic"
+            className="first_bio_pic"
+          ></img>
+          <div className="collage_column">
+            <img
+              src={second_pic_collage}
+              alt="2nd_pic"
+              className="second_bio_pic"
+            ></img>
+            <img
+              src={third_pic_collage}
+              alt="3rd_pic"
+              className="third_bio_pic"
+            ></img>
+          </div>
+        </div>
       </div>
     </div>
   );
