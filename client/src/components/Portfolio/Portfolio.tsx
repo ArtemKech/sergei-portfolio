7d0982464628ee639849bfe0cfee1361f8497216
ArtemKech <kecha1@student.op.ac.nz>
2024-04-11 21:05:24 +1200

    feat(client/portfolio): created sections for the portfolio with responsive design. Should add buttons with links later

--- client/src/components/Portfolio/Portfolio.tsx
+++ client/src/components/Portfolio/Portfolio.tsx
@@ -8,54 +8,66 @@ import private_photoshoot from "../../assets/Portfolio/MainSlider/private_photos
 import school_photoshoot from "../../assets/Portfolio/MainSlider/school_photoshoot.png";
 
 const Portfolio: React.FC = () => {
-  const images = [
+  interface Images {
+    name: string;
+    image: string;
+    link: string;
+    description: string;
+  }
+  const images: Images[] = [
     {
-      name: "Custom",
+      name: "Тематический",
       image: custom_photoshoot,
       link: "/portfolio/custom_photoshoot",
+      description:
+        "Тематический фотосет это уникальная возможность создать неповторимые фотографии в стиле, который вам нравится. Это может быть фотосессия в стиле ретро, стимпанк, киберпанк, фэнтези, косплей и многое другое. Ваша фантазия не имеет границ, и я помогу вам воплотить ваши идеи в жизнь.",
     },
     {
-      name: "Group",
+      name: "Групповой",
       image: group_photoshoot,
       link: "/portofolio/group_photoshoot",
+      description:
+        "Групповой фотосет это отличная возможность создать красивые и яркие фотографии вместе с друзьями или коллегами. Это может быть фотосессия на природе, в студии или в любом другом месте, которое вам нравится. Я помогу вам выбрать лучший вариант и создать красивые фотографии, которые вы будете с удовольствием смотреть и показывать друзьям.",
     },
     {
-      name: "School",
+      name: "Школьный",
       image: school_photoshoot,
       link: "/portfolio/school_photoshoot",
+      description:
+        "Школьный фотосет завпечатлит ваши самые памятные моменты в школе. Это может быть фотосессия на выпускном вечере, в день знаний или в любой другой день, который вам нравится. Я помогу вам создать красивые и яркие фотографии, которые вы будете с удовольствием смотреть и показывать друзьям.",
     },
     {
-      name: "New Year",
+      name: "Новый Год",
       image: newyear_photoshoot,
       link: "/portfolio/newyear_photoshoot",
+      description:
+        "В предверии нового года я предлагаю вам уникальную возможность создать красивые и яркие фотографии, которые станут отличным подарком для вас и ваших близких. Это может быть фотосессия в студии, на улице или в любом другом месте, которое вам нравится. Я помогу вам выбрать лучший вариант и создать красивые фотографии, которые вы будете с удовольствием смотреть и показывать друзьям.",
     },
     {
-      name: "Private",
+      name: "Индивидуальный",
       image: private_photoshoot,
       link: "/portfolio/private_photoshoot",
+      description:
+        "Здесь могут быть ваши фотографии на любую тему и в любом стиле. Ваши идеи и мои навыки помогут вам создать уникальные и красивые фотографии, которые вы будете с удовольствием смотреть и показывать друзьям.",
     },
     {
-      name: "Halloween",
+      name: "Праздничный",
       image: halloween_photoshoot,
       link: "/portfolio/halloween_photoshoot",
+      description:
+        "Праздничный фотосет всегда радует глаз и создает праздничное настроение. Это может быть фотосессия на Хэллоуин, на День Рождения, на 8 Марта или на любой другой праздник, который вам нравится. Я помогу вам создать красивые и яркие фотографии, которые вы будете с удовольствием смотреть и показывать друзьям.",
     },
   ];
 
   return (
-    <div>
-      <ul className="port_container">
-        {images.map((item, index) => (
-          <li key={index} className="port_section">
-            <a href={item.link} className="port_section_link">
-              <img
-                src={item.image}
-                alt={item.name}
-                className="port_section_pic"
-              />
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div className="port_container">
+      {images.map((item, index) => (
+        <div key={index} className="port_section">
+          <img src={item.image} alt={item.name} className="port_section_pic" />
+          <span className="port_section_name">{item.name}</span>
+          <div className="port_section_description">{item.description}</div>
+        </div>
+      ))}
     </div>
   );
 };
